5d1d3cd38e581293ac34be9ad20098d2e0223e72
JPregliasco <91738384+juanpi8345@users.noreply.github.com>
2022-10-23 18:09:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,308 +3,96 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Restaurante Y - Bienvenidos</title>
+	<title>Contacto</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="libs/wow/animate.css">
 	<link rel="stylesheet" href="libs/owl/owl.carousel.css">
 	<link rel="stylesheet" href="libs/owl/owl.theme.default.css">
 	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 	<script src="https://kit.fontawesome.com/eb09d1be43.js" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="js/jQuery.js"></script>
-	<script type="text/javascript" src="js/funciones.js"></script>
+	<script src="libs/wow/wow.js"></script>
 	<script src="libs/owl/owl.carousel.js"></script>
 </head>
-<body>
+<body class="desaparecido">
 	<header id="cabecera">
 		<div class="container">
 			<h2>Restaurante Y</h2>
 			<nav class="menu">
-				<a href="#nosotros">Nosotros</a>
-				<a href="#carta">Comidas</a>
-				<a href="#postres">Postres</a>
-				<a href="#servicios">Servicios</a>
-				<a href="#testimonios">Testimonios</a>
-				<a href="">Contacto</a>
+				<a href="index.html#nosotros">Nosotros</a>
+				<a href="index.html#carta">Comidas</a>
+				<a href="index.html#postres">Postres</a>
+				<a href="index.html#servicios">Servicios</a>
+				<a href="index.html#testimonios">Testimonios</a>
+				<a href="contacto.html">Contacto</a>
 			</nav>
-			<a href="" class="bars"><i class="fa-solid fa-bars"></i></a>
-		</div>
-	</header>
-	<main>
-		<section id="banner">
-			<div class="img-banner"></div>
+		<a href="" class="bars"><i class="fa-solid fa-bars"></i></a>
+	</div>
+</header>
+<main>
+	<section id="banner-contacto">
+			<div class="trama-contacto"></div>
 			<div class="container">
-				<div class="fila alinear-texto">
-					<div class="columna columna-50 columna-mb-100">
-						<h1>Restaurante Y - Bienvenidos</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<a class= "btn btn-transparente ver-mas" href="#">Ver mas</a>
-					</div>	
-				</div>
-			</div>
-		</section>
-		<section id= "nosotros">
-			<div class="container-full sin-padding">
 				<div class="columna columna-100">
-					<div class="fila">
-						<div class="columna columna-50 sin-padding nosotros alinear-texto columna-mb-100">
-							<h2 class="titulos">Nuestra historia</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-							proident, sunt in culpa qui officia deserunt mollit anim id est laborum incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat..</p>
-				  		</div>
-						<div class="columna columna-50 sin-padding imagen columna-mb-100">
-							<img src="img/nosotros.jpg" width="5472" height="3648">
-						</div>
-					</div>
-				</div>	
-			<div>
-		</section>
-		<section id="carta">
-			<div class="container-full">
-			    <div class="fila">
-					<div class="columna columna-100">
-						<h2 class="titulos">Comidas</h2>
-					</div>
-				</div>	
-				<div class="fila">
-					<div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/pollo.jpg" width="1920" height="1080">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Carnes</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
-				  <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/hamburguesa.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Hamburguesas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
-				  <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/ensalada.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Ensaladas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
+					<h1 class="titulos wow slideInLeft" data-wow-duration="2s">Contacto</h1>
+				</div>
 			</div>
+	</section>
+	<section id="contacto">
+		<div class="container-full">
 			<div class="fila">
-					<div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/pizza.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Pizzas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
-				  <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/pasta.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Pastas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
-				  <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/milanesa.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Milanesas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-				  </div>
-			</div>
-		</section>
-		<section id="postres">
-			<div class="container">
-				<div class="fila">
-					<div class="columna columna-100 ">
-						<h2 class="titulos">Postres</h2>
-					</div>
+				<div class="columna columna-50 columna-mb-100 sin-padding none">
+					<iframe width="700" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=RestauranteY+(RestauranteY)&amp;ie=UTF8&amp;t=&amp;z=2&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 				</div>
-			</div>
-			<div class="container-full">
-				<div class="fila">
-					<div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/frutillas con crema.jpg" width="1920" height="1080">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Frutillas con crema</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-					 <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/mousse.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Mousse de chocolate</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-					 <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/torta.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Tortas</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-				</div>
-				<div class="fila">
-					<div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/helado.jpg" width="1920" height="1080">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Helado</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-					 <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/Cupcake.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Cupcakes</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-					 <div class="columna columna-33 sin-padding columna-mb-100">
-						<div class="contenedor-imagen">
-							<img src="img/brownie.jpg">
-							<div class="contenido-imagen alinear-texto">
-								<h2 class="titulos">Brownie</h2>
-								<a href="" class="btn btn-transparente">Ver mas</a>
-							</div>
-						</div>		
-					 </div>
-				</div>
-			</div>	
-		</section>
-		<section id="servicios" class="seccion">
-			<div class="container">
-				<div class="fila">
-					<div class="columna columna-100">
-						<h2 class="titulos">Servicios</h2>
-					</div>
-				</div>
-				<div class="fila space-around margin-arriba">
-					<div class="columna columna-20 alinear-texto padding-arriba-abajo contenedor-servicios columna-mb-100 s">
-						<i class="fa-solid fa-truck"></i>
-						<h4>Delivery</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</p>
-					</div>
-					<div class="columna columna-20 alinear-texto padding-arriba-abajo contenedor-servicios columna-mb-100 ">
-						<i class="fa-solid fa-face-smile"></i>
-						<h4>Excelente atencion al cliente</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</p>
-					</div>
-					<div class="columna columna-20 alinear-texto padding-arriba-abajo contenedor-servicios columna-mb-100 ">
-						<i class="fa-solid fa-hand-holding-dollar"></i>
-						<h4>Precio y calidad</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.</p>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section id="testimonios" class="seccion">
-			<div class="container-full">
-				<div class="fila fila-centrada">
-					<div class="contenedor-testimonios"></div>
-					<div class="trama-testimonios"></div>
-					<div class="columna columna-50 alinear-texto columna-mb-100">
-						<h2 class="titulos">Testimonios</h2>
-							<div class="owl-carousel owl-theme">
-								<div class="item">
-									<h4>Testimonio 1</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-							</div>
-								<div class="item">
-									<h4>Testimonio 2</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in 	voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-								</div>
-								<div class="item">
-									<h4>Testimonio 3</h4>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-									consequat. Duis aute irure dolor in reprehenderit in 	voluptate velit esse
-									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-									proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-								</div>
+				<div class="columna columna-50 seccion columna-mb-100">
+					<h2 class="titulos wow slideInLeft" data-wow-duration="2s">Contactanos</h2>
+					<form class="frmcontacto alinear-texto" method="post">
+						<div class="form-bloques">
+							<input type="text" name="nombre" class="input" placeholder="Nombre" minlength="3" required>
 						</div>
-					</div>
+						<div class="form-bloques">
+							<input type="email" name="email" class="input" placeholder="Email" minlength="7" required>
+						</div>
+						<div class="form-bloques">
+							<input type="text" name="telefono" class="input" placeholder="Telefono" minlength="7" required>
+						</div>
+						<div class="form-bloques">
+							<input type="text" name="asunto" class="input" placeholder="Asunto" minlength="7" required>
+						</div>
+						<div class="form-bloques">
+							<textarea  type="text" name="mensaje" class="input" placeholder="Mensaje" minlength="10" required></textarea>
+						</div>
+						<a class= "btn btn-transparente ver-mas" href="#nosotros">Enviar</a>
+					</form>
 				</div>
 			</div>
-		</section>
-		<div class="trama">
-			<div class="cuerpo-imagen">
-				<img src="">
-				<a href=""><i class="fa-solid fa-close"></i></a>
-			</div>
 		</div>
-	</main>
-	<footer>
-		<div class="container">
-			<div class="fila fila-centrada">
-					<div class="columna columna-50 alinear-texto ">
-						<p>Hecho con <i class="fa-solid fa-heart corazon"></i> por Juan</p>
-				  </div>
-				  <div class="columna columna-50 alinear-texto">
-				  	<nav>
-				  		<a href=""><i class="fa-brands fa-instagram"></i></a>
-				  		<a href=""><i class="fa-brands fa-twitter"></i></a>
-				  		<a href=""><i class="fa-solid fa-envelope"></i></a>
-				  		<a href=""><i class="fa-brands fa-facebook"></i></a>
-				  	</nav>
-				  </div>
-			</div>
+	</section>
+</main>
+<footer>
+	<div class="container">
+		<div class="fila fila-centrada">
+				<div class="columna columna-50 alinear-texto ">
+					<p>Hecho con <i class="fa-solid fa-heart corazon"></i> por Juan</p>
+				 </div>
+				 <div class="columna columna-50 alinear-texto">
+				  <nav>
+				  	<a href=""><i class="fa-brands fa-instagram"></i></a>
+				  	<a href=""><i class="fa-brands fa-twitter"></i></a>
+				  	<a href=""><i class="fa-solid fa-envelope"></i></a>
+				  	<a href=""><i class="fa-brands fa-facebook"></i></a>
+				  </nav>
+			 </div>
 		</div>
-	</footer>
+	</div>
+</footer>
+<div class="trama2">
+	<h2 class="titulos">Restaurante Y</h2>
+</div>
+<script type="text/javascript" src="js/funciones.js"></script>
 </body>
 </html>
